7f51ff95f859c3c5b045af26a31dee3a8f59b68e
Yevhen Martynenko <marty@Yevhens-MBP-2.lan>
2020-07-04 11:24:57 +0300

    SC-1 split settings for project

--- src/config/settings.py
+++ src/config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'todo.urls'
+ROOT_URLCONF = 'router'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'todo.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
